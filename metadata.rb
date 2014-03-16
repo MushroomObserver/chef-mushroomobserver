995805a700496b7c2901d46dc82df7d89c619c8e
Nathan Wilson <nathan@collectivesource.com>
2014-03-16 14:36:23 -0400

    Incremented cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nathan@mushroomobserver.org'
 license          'MIT'
 description      'Installs/Configures mushroomobserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ rvm mysql database build-essential sudo }.each do |cb|
   depends cb
